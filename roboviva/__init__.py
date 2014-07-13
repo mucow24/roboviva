bc8ca25af0babbaab57e610db287b698897a18bd
Mike Kocurek <mike@snuggl.es>
2014-07-13 13:24:46 -0400

    added app prefix

--- roboviva/__init__.py
+++ roboviva/__init__.py
@@ -3,5 +3,5 @@ from .views import blueprint
 
 app = Flask(__name__)
 app.config.from_object('config')
-app.register_blueprint(blueprint)
+app.register_blueprint(blueprint, url_prefix="/roboviva")
 
