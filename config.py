476e79ccf0d1bf4490cbaa27cc726211777aa4f8
Mike Kocurek <mike@snuggl.es>
2014-07-16 07:24:06 -0400

    config.py fix

--- config.py
+++ config.py
@@ -2,6 +2,5 @@ import os
 
 _cwd = os.path.dirname(os.path.abspath(__file__))
 
-# PDF_CACHE_DIR = os.path.join(_cwd, 'pdf_cache')
-PDF_CACHE_DIR = '/home/nginx-data/python_apps/roboviva/pdf_cache'
+PDF_CACHE_DIR = os.path.join(_cwd, 'pdf_cache')
 SHELVE_FILENAME = '/tmp/roboviva.db'
