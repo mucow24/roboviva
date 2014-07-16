d8b90f6a53238f8b2a8484568f7ad3f7b36ed981
Mike Kocurek <mike@snuggl.es>
2014-07-16 09:08:06 -0400

    log to stderr

--- roboviva/__init__.py
+++ roboviva/__init__.py
@@ -3,10 +3,20 @@ from flask.ext import shelve
 from .views import blueprint
 
 import sys
+import logging
+
+# Get ourselves into utf8 mode, so we don't have ascii / unicode headaches:
 reload(sys)
 sys.setdefaultencoding("utf8")
 
 app = Flask(__name__)
 app.config.from_object('config')
-app.register_blueprint(blueprint, url_prefix="/roboviva")
+app.register_blueprint(blueprint,
+                       url_prefix="/roboviva")
+
+if not app.debug:
+  # In production mode, add log handler to sys.stderr.
+  app.logger.addHandler(logging.StreamHandler())
+  app.logger.setLevel(logging.WARN)
+
 shelve.init_app(app)
